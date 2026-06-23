5294ceee6bef4fce8f558537416bcd7a6bb1e729
Mansoor <shafiq@Shafiqs-MacBook-Air.local>
2026-06-23 17:36:49 +0300

    Fix Render startup

--- main.py
+++ main.py
@@ -248,4 +248,8 @@ def delete_chat(chat_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
